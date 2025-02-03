c7479aa2a6cb168a4b41e91f317c2cd9c561df0f
Gabriel Diaz Iturry <gabriel.diaz.iturry@gmail.com>
2025-02-03 16:14:27 -0400

    code for extended test

---
+++ README_EXTENDED_TESTS.md
@@ -0,0 +1,9 @@
+# Extended Tests
+Here we have some additional tests.
+We have additional data and comparisions to Fortran outputs.
+
+DO NOT MAKE A PR TO MAIN FROM THIS BRANCH!
+If some tests does not pass write the code in another branch like `dev-stuff` 
+then merge `dev-stuff` to this branch `extended-tests`, and make sure that all the tests pass.
+the make a PR from `dev-stuff`, make sure to do not pass the additinal data and tests to the 
+`main` branch.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,19 @@
 using SafeTestsets
 
-@safetestset "Initial Settings" begin include("initialsettings_tests.jl") end
+# @safetestset "Initial Settings" begin include("initialsettings_tests.jl") end
+#
+# @safetestset "Initialize Run Part 1" begin include("initialize_runpart1_tests.jl") end
+#
+# @safetestset "TOML Integration" begin include("readtoml_tests.jl") end
+#
+# @safetestset "Initialize Run Part 2" begin include("initialize_runpart2_tests.jl") end
+#
+# @safetestset "Filemanagement" begin include("filemanagement_tests.jl") end
+#
+# @safetestset "Other Runs" begin include("other_runs_tests.jl") end
+#
+# @safetestset "API" begin include("api_tests.jl") end
+#
+# @safetestset "API advanced" begin include("api_advanced_tests.jl") end
 
-@safetestset "Initialize Run Part 1" begin include("initialize_runpart1_tests.jl") end
-
-@safetestset "TOML Integration" begin include("readtoml_tests.jl") end
-
-@safetestset "Initialize Run Part 2" begin include("initialize_runpart2_tests.jl") end
-
-@safetestset "Filemanagement" begin include("filemanagement_tests.jl") end
-
-@safetestset "Other Runs" begin include("other_runs_tests.jl") end
-
-@safetestset "API" begin include("api_tests.jl") end
-
-@safetestset "API advanced" begin include("api_advanced_tests.jl") end
+@safetestset "Extended Test 1" begin include("./extended_test/extended_test_1.jl") end
